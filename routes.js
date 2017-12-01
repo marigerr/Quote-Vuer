0726b9c2fa154bd24bcb83b55c68592acc6e58f7
marigerr <marigerr.sv@gmail.com>
2017-12-01 18:48:40 +0100

    factor out routes to separate file

---
+++ routes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = new express.Router();
+const Quote = require('./models/quote.js');
+// const path = require('path');
+
+// random quote api route
+router.route("/api.quotes/random")
+  .get((req, res) => {
+    Quote.count().exec(function (err, count) {
+      const random = Math.floor(Math.random() * count);
+      Quote.findOne().skip(random).exec(
+        (err, quote) => {
+          if (err) console.log(err);
+          else {
+            res.json(quote);
+          }
+        });
+    });
+  });
+
+// Root address of app will display index.html with api information
+router.route("/")
+  .get((req, res) => {
+    console.log('hi');
+    res.sendFile(__dirname + '/views/index.html');
+  });
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 require('dotenv').config(); 
 const express = require('express');
+const routes = require('./routes');
 const cors = require('cors');
 const app = express();
-const Quote = require('./models/quote.js');
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
@@ -16,33 +16,11 @@ db.on('error', console.error.bind(console, 'connection error:'));
 
 //  Cross origin resource sharing (CORS)
 app.use(cors());
-
 // Serves static files from public folder
 app.use(express.static('public'));
 
 /**** Routes ****/
-
-// Root address of app will display index.html with api information
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-
-// random quote api route
-app.get("/api.quotes/random", function (request, response) {
-    
-  Quote.count().exec(function (err, count) {
-    
-    var random = Math.floor(Math.random() * count);
-    Quote.findOne().skip(random).exec(
-      function (err, result) {
-        if(err) console.log(err);
-        else {
-          response.json(result);
-        }
-      });
-  });  
-});
-
+app.use('/', routes);
 
 // start the server
 const listener = app.listen(process.env.PORT, function () {
